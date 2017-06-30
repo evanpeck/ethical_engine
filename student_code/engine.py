9a47d37a235f4962e31804ad3d15c6e7b8a1bc4c
Evan Peck <evanpeck@gmail.com>
2017-06-30 09:51:36 -0400

    Added engine template, added author info

---
+++ student_code/engine.py
@@ -0,0 +1,39 @@
+'''
+Provides template code to automatically make decisions for the autonomous
+car.
+'''
+from scenario import *
+
+def decide(scenario):
+    """ Decides whether your car will save the passengers or pedestrians
+
+    This method is typically called from a DIFFERENT python file. Given
+    any scenario
+
+    Args:
+        scenario: a scenario object from scenario.py. This object contains
+            all of the information about the scenario. You can see some
+            examples below in the sample code.
+
+    Returns:
+        A string which indicates whether you are saving "passengers" or
+        "pedestrians". Note that your method MUST make a decision for
+        ANY potential scenario.
+    """
+
+    # NOTE: YOU NEED TO REPLACE ALL OF THE CODE BELOW!!!
+    # This simply demonstrates how to access information from the scenario.
+    print("Are the pedestrians crossing legally?", scenario.legalCrossing)
+    print("Is the car in the same lane as the pedestrians?", scenario.pedsInLane)
+    print("Are you in car?", scenario.you)
+
+    print("Print whether each pedestrian is a human, animal, or you")
+    for person in scenario.pedestrians:
+        print(person.charType)
+
+
+    # Your program must choose to save either pedestrians or passengers
+    if len(scenario.passengers) > len(scenario.pedestrians):
+        return "passengers"
+    else:
+        return "pedestrians"
