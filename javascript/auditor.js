852eef52cfd5a215c5340bb70590fe23877d9f36
lfernandez55 <lfernandez@weber.edu>
2021-06-03 16:24:52 -0600

    Console.log formatted, with attributes and subattributes

--- javascript/auditor.js
+++ javascript/auditor.js
@@ -9,53 +9,74 @@ function audit(){
 
 function Audit(){
     this.biasMetrics = {
-        pedestrians : {live:0,die:0},
-        passengers : {live:0,die:0},
+        // pedestrians : {live:0,die:0},
+        // passengers : {live:0,die:0},
     
         //charType
-        human : {live:0,die:0},
-        you : {live:0,die:0},
-        dog : {live:0,die:0},
-        cat : {live:0,die:0},
+        charType : {
+            human : {live:0,die:0},
+            you : {live:0,die:0},
+            dog : {live:0,die:0},
+            cat : {live:0,die:0},
+        },
 
         //age
-        baby : {live:0,die:0},
-        child : {live:0,die:0},
-        adult : {live:0,die:0},
-        elderly : {live:0,die:0},
-    
+        age: {
+            baby : {live:0,die:0},
+            child : {live:0,die:0},
+            adult : {live:0,die:0},
+            elderly : {live:0,die:0},
+        },
+
+
         //gender
-        female : {live:0,die:0},
-        male : {live:0,die:0},
-        
+        gender: {
+            female : {live:0,die:0},
+            male : {live:0,die:0},
+        },
+
+
         //bodyType
-        overweight : {live:0,die:0},
-        athletic : {live:0,die:0},
-        average : {live:0,die:0},
-    
+        bodyType: {
+            overweight : {live:0,die:0},
+            athletic : {live:0,die:0},
+            average : {live:0,die:0},
+        },
+
+
         //pregnant
-        pregnant : {live:0,die:0},
+        pregnant: {
+            true: {live:0,die:0},
+            false: {live:0,die:0},
+        },
+
 
         //profession
-        doctor : {live:0,die:0},
-        CEO : {live:0,die:0},
-        criminal : {live:0,die:0},
-        homeless : {live:0,die:0},
-        unemployed : {live:0,die:0},
-        unknown : {live:0,die:0},
+        profession: {
+            doctor : {live:0,die:0},
+            CEO : {live:0,die:0},
+            criminal : {live:0,die:0},
+            homeless : {live:0,die:0},
+            unemployed : {live:0,die:0},
+            unknown : {live:0,die:0},
+        },
 
+        misc: {
+            pedestrians : {live:0,die:0},
+            passengers : {live:0,die:0},
+        }
         //
-        pedsInLaneTrue : {live:0,die:0}, // this only counts pedestrian lives and deaths 
-        pedsInLaneFalse : {live:0,die:0}, // this only counts pedestrian lives and deaths 
+    //     pedsInLaneTrue : {live:0,die:0}, // this only counts pedestrian lives and deaths 
+    //     pedsInLaneFalse : {live:0,die:0}, // this only counts pedestrian lives and deaths 
 
 
-        undefined : {live:0,die:0} // this attribute aggregates data like pregnant.undefined profession.undefined etc 
-    }
+    //     undefined : {live:0,die:0} // this attribute aggregates data like pregnant.undefined profession.undefined etc 
+    },
 
     this.simulations = 0
 
     this.runSimulation = function(){
-        while(this.simulations < 1000){
+        while(this.simulations < 100){
             let scenario = new Scenario()
             let result = decide(scenario)
 
@@ -71,49 +92,55 @@ function Audit(){
             if (result == "passengers"){
                 for (let x=0; x < scenario.passengers.length; x++) {
                     let person = scenario.passengers[x]
-                    this.aggregateResults("live",person)
-                    this.biasMetrics.passengers.live += 1
+                    this.aggregateResults("live",person,"passengers")
+                    // this.biasMetrics.passengers.live += 1
                 }
                 for (let x=0; x < scenario.pedestrians.length; x++) {
                     let person = scenario.pedestrians[x]
-                    this.aggregateResults("die",person)
-                    this.biasMetrics.pedestrians.die += 1
-                    this.biasMetrics[pedsInLaneKey].die += 1
+                    this.aggregateResults("die",person,"pedestrians")
+                    // this.biasMetrics.pedestrians.die += 1
+                    // this.biasMetrics[pedsInLaneKey].die += 1
                 }
             }
             else{
                 for (let x=0; x < scenario.pedestrians.length; x++) {
                     let person = scenario.pedestrians[x]
-                    this.aggregateResults("live",person)
-                    this.biasMetrics.pedestrians.live += 1
-                    this.biasMetrics[pedsInLaneKey].live += 1
+                    this.aggregateResults("live",person,"pedestrians")
+                    // this.biasMetrics.pedestrians.live += 1
+                    // this.biasMetrics[pedsInLaneKey].live += 1
                 }
                 for (let x=0; x < scenario.passengers.length; x++) {
                     let person = scenario.passengers[x]
-                    this.aggregateResults("die",person)
-                    this.biasMetrics.passengers.die += 1
+                    this.aggregateResults("die",person,"passengers")
+                    // this.biasMetrics.passengers.die += 1
                 }
             }
 
             this.simulations = this.simulations + 1
             console.log("Simulations: ", this.simulations)
         }
-        for (var identity in this.biasMetrics) {
-            this.biasMetrics[identity].percentLive = this.biasMetrics[identity].live / (this.biasMetrics[identity].live + this.biasMetrics[identity].die)
+        for (var attribute in this.biasMetrics) {
+            // this.biasMetrics[identity].percentLive = this.biasMetrics[identity].live / (this.biasMetrics[identity].live + this.biasMetrics[identity].die)
+            console.log("ATTTR:", attribute)
+            for (var subAttr in this.biasMetrics[attribute]) {
+                console.log("RRRRRRRRRRRR:", subAttr)
+                this.biasMetrics[attribute][subAttr].percentLive = this.biasMetrics[attribute][subAttr].live / (this.biasMetrics[attribute][subAttr].live + this.biasMetrics[attribute][subAttr].die)
+                
+            }
         }
     }
 
-    this.aggregateResults = function (liveDie, person){
-        for (var identity in person) {
-            if(typeof person[identity] == "string"){
-                // run on all keys except for person.stringRep function and person.pregnant boolean
-                this.biasMetrics[person[identity]][liveDie] += 1
+    this.aggregateResults = function (liveDie, person, passOrPedes){
+        for (var attribute in person) {
+            //console.log("attribute:", attribute)
+            if(attribute != "stringRep"){
+                // run on all keys except for person.stringRep function 
+                this.biasMetrics[attribute][person[attribute]][liveDie] += 1
             }
-            if(typeof person[identity] == "boolean"){
-                // run on person.pregnant boolean
-                this.biasMetrics.pregnant[liveDie] += 1
-            }
+            
         }
+        console.log("GGGGGGGGGGG",passOrPedes, this.biasMetrics.misc[passOrPedes])
+        this.biasMetrics.misc[passOrPedes][liveDie] += 1
     }
 
 
@@ -123,9 +150,21 @@ function Audit(){
         console.log("except for: 1) Ratios titled 'pedsInLaneFalse, pedsInLaneTrue,  pedestrians' which just")
         console.log("use pedestrians lives and deaths. 2) Ratio titled 'passengers' which just uses passengers lives and deaths)")
         let sortable = []
-        for (var identity in this.biasMetrics) {
-            sortable.push([identity,this.biasMetrics[identity]])
+        // for (var attribute in this.biasMetrics) {
+        //     // this.biasMetrics[attribute][person[attribute]][liveDie]
+        //     // sortable.push([identity,this.biasMetrics[identity]])
+        //     console.log("ZZZZZZZZZZZ", attribute)
+        //     sortable.push([attribute,this.biasMetrics[attribute][person[attribute]]])
+        // }
+        for (var attribute in this.biasMetrics) {
+             for (var subAttr in this.biasMetrics[attribute]) {
+                // this.biasMetrics[attribute][subAttr].percentLive = this.biasMetrics[attribute][subAttr].live / (this.biasMetrics[attribute][subAttr].live + this.biasMetrics[attribute][subAttr].die)
+                sortable.push([attribute + "=" + subAttr,this.biasMetrics[attribute][subAttr]])
+            }
         }
+
+
+
         sortable.sort(function(a,b){
             return a[1].percentLive - b[1].percentLive
         })
@@ -137,7 +176,7 @@ function Audit(){
                 let attribute = e[0]
                 let percentSaved = (e[1].percentLive *100).toFixed(0)
                 let encountered = e[1].live + e[1].die
-                let spaceNum = 20 - e[0].length
+                let spaceNum = 21 - e[0].length
                 let spaces = ""
                 for(let x=0; x < spaceNum; x++){
                     spaces = spaces + " "
